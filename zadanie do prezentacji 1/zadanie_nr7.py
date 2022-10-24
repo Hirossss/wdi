ebb84311ce1e40d25ce7fa1b2c0617c782c13104
Nikodem <nikodemjakubowski.nj@gmail.com>
2022-10-24 17:30:40 +0200

    ulepszona wersja z komentarzami

--- zadanie do prezentacji 1/zadanie_nr7.py
+++ zadanie do prezentacji 1/zadanie_nr7.py
@@ -1,16 +1,16 @@
 def dzielenie(liczba):
     dzielniki=[]
     if liczba>0:
-        for dzielnik in range(1,int(liczba/2)+1):   #sprawdzamy  w zakresie do polowy liczby bo dalej sa wartosci niecalkowite
+        for dzielnik in range(1,int(liczba/2)+1):   #sprawdzamy w zakresie do połowy liczby, bo dalej są wartości niecałkowite
             if liczba % dzielnik==0:
-                dzielniki.append(dzielnik)
+                dzielniki.append(dzielnik)          #dzielniki aplikujemy do listy, żeby wynik programu ładnie wyglądał
                 dzielniki.append(dzielnik*-1)
-        dzielniki.append(liczba)
+        dzielniki.append(liczba)                    #uwzględniamy również samą liczbę, bo nie "przechodzi" ona przez petlę
         dzielniki.append(liczba*-1)
         dzielniki.sort()
         print(str(dzielniki))
     elif liczba<0:
-        for dzielnik in range(int(liczba/2), 0):
+        for dzielnik in range(int(liczba/2), 0):    #wszystkie dzieje sie analogicznie, ale istotna jest kolejność zakresów!
             if liczba % dzielnik==0:
                 dzielniki.append(dzielnik)
                 dzielniki.append(dzielnik * -1)
@@ -22,13 +22,13 @@ def dzielenie(liczba):
         print("0 ma nieskończenie wiele dzielników")
 
 run=True
-while(run):
+while(run):         #nieskończona pętla z instrukcją try, except, else, aby zoptymalizować użytkowanie programu
     x = input("Wprowadz swoją liczbę, a program wypisze jej całkowite dzielniki: ")
     try:
-        val = int(x)
+        val = int(x)    #sprawdzamy czy input jest liczbą całkowitą zgodnie z przeznaczeniem programu, zabezpieczamy się przed użyciem złej zmiennej
     except ValueError:
-        print("Nie wprowadzono liczby całkowitej, proszę spróbować ponownie.  ")
+        print("Nie wprowadzono liczby całkowitej, proszę spróbować ponownie. ")  #run jest nadal True i użytkownik może ponownie wprowadzać wartości do x aż wprowadzi argument całkowity
     else:
-        x=int(x)    #program wyrzucal definicje, bo traktowal x zdefiniowanego w lini 26 jako stringa
-        run=False
-        dzielenie(x)
+        x=int(x)    #trzeba dorzucić typ int, bo funkcja dzielenie nie zadziała ze stringiem określonym w lini 26
+        run=False   #nie chcemy, żeby pętla się dalej wykonywała
+        dzielenie(x)    #wywołujemy funkcję i koniec
