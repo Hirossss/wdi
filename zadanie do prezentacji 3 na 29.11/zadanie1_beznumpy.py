8c2ef0683d3949d6ff182153cd11e65663a2944c
Nikodem <nikodemjakubowski.nj@gmail.com>
2022-11-29 16:00:44 +0100

    zadania na 29.11

--- zadanie do prezentacji 3 na 29.11/zadanie1_beznumpy.py
+++ zadanie do prezentacji 3 na 29.11/zadanie1_beznumpy.py
@@ -13,9 +13,21 @@ def tworzenieMacierzy(w, k, dane):
             listaWierszy.append(dane[w * i + j])#niestety nie uzywamy wszystkich liczb z "dane", ale ciezko to zrobic bardziej optymalnie
         mat.append(listaWierszy)
     return mat
+run=True
+while(run):
+    wiersze=input("Prosze podac ilosc wierszy: ")
+    kolumny=input("Prosze podac ilosc kolumn: ")
+    try:
+        x=int(wiersze)
+        y=int(kolumny)
+    except ValueError:
+        print("Prosze podac parametry ponownie")
+    else:
+        run=False
 
-wiersze=int(input("Prosze podac ilosc wierszy: "))
-kolumny=int(input("Prosze podac ilosc kolumn: "))
+wiersze=int(wiersze)
+kolumny=int(kolumny)
+
 if wiersze > kolumny:
     n=wiersze
 elif kolumny > wiersze:
@@ -29,9 +41,14 @@ for i in range(0, n*n):
     liczbyLosowe2.append(random.randint(0, 9))
 
 mat1= tworzenieMacierzy(wiersze,kolumny,liczbyLosowe1)
-print(mat1)
 mat2= tworzenieMacierzy(wiersze,kolumny,liczbyLosowe2)
-print(mat2)
+
+print("To jest pierwsza macierz. ")
+for p in range(wiersze):
+    print(mat1[p])
+print("To jest druga macierz. ")
+for l in range(wiersze):
+    print(mat2[l])
 
 wynik_dod=[[0]*kolumny for i in range(wiersze)] #wypelniamy dana liste zerem * ilos kolumn, powtarzamy dla n wierszy
 wynik_odej=[[0]*kolumny for i in range(wiersze)]
@@ -48,8 +65,8 @@ print("Wynik odejmowania macierzy to macierz: ")
 for j in range(wiersze):
     print(wynik_odej[j])
 '''
-PRZYPADKI TESTOWE I WYJASNIENIA:
-
+PRZYPADKI TESTOWE
+1. Zabezpieczenie przed podaniem zlych parametrow.
 '''
 
 
